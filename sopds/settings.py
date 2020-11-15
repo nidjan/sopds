7835edbcc42b4f7d41b8e8ae63e28f25c83e3cff
nidjan <38748937+nidjan@users.noreply.github.com>
2020-11-16 01:57:11 +0800

    Update settings.py

--- sopds/settings.py
+++ sopds/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'sopds.wsgi.application'
 #    }             
 #}
 
-#DATABASES = {
-#    'default': {
-#    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#    'NAME': 'sopds',
-#    'USER': 'sopds',
-#    'PASSWORD': 'sopds',
-#    'HOST': '', # Set to empty string for localhost.
-#    'PORT': '', # Set to empty string for default.
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }         
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'sopds',
+    'USER': 'sopds',
+    'PASSWORD': 'sopds',
+    'HOST': '', # Set to empty string for localhost.
+    'PORT': '', # Set to empty string for default.
+    }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }         
+#}
 
 #### SOPDS DATABASE SETTINGS FINISH ####
 
